25aba7c156c5e1c555aa8121e3374c1b24783455
Skydark Chen <skydark2@gmail.com>
2012-07-19 11:38:55 +0800

    update 10.py

--- 010.py
+++ 010.py
@@ -13,17 +13,20 @@ MAX = 2000000
 from time import time; t=time()
 from mathplus import isqrt
 
-marked = [0] * MAX
-value = 3
-s = 2
-limit = isqrt(MAX)+1
-while value < limit:
-    if marked[value] == 0:
-        s += value
-        for i in range(value*3, MAX, value*2):
-            marked[i] = 1
-    value += 2
-for i in range(limit//2*2, MAX, 2):
-    marked[i] = 1
-s += sum(p+limit for p, f in enumerate(marked[limit:]) if f == 0)
-print(s)#, time()-t
+def pe10(MAX):
+    marked = [0] * MAX
+    value = 3
+    s = 2
+    limit = isqrt(MAX)+1
+    while value < limit:
+        if marked[value] == 0:
+            s += value
+            for i in range(value*3, MAX, value*2):
+                marked[i] = 1
+        value += 2
+    for i in range(limit//2*2+1, MAX, 2):
+        if marked[i] == 0:
+            s += i
+    return s
+
+print(pe10(MAX))#, time()-t)
