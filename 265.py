0505002dba54ba943943907055a3e5e81585330a
Skydark Chen <skydark2@gmail.com>
2012-11-16 23:48:23 +0800

    solved problem 265

---
+++ 265.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# 2N binary digits can be placed in a circle so that all the N-digit clockwise subsequences are distinct.
+
+# For N=3, two such circular arrangements are possible, ignoring rotations:
+
+# For the first arrangement, the 3-digit subsequences, in clockwise order, are:
+# 000, 001, 010, 101, 011, 111, 110 and 100.
+
+# Each circular arrangement can be encoded as a number by concatenating the binary digits starting with the subsequence of all zeros as the most significant bits and proceeding clockwise. The two arrangements for N=3 are thus represented as 23 and 29:
+# 00010111 2 = 23
+# 00011101 2 = 29
+
+# Calling S(N) the sum of the unique numeric representations, we can see that S(3) = 23 + 29 = 52.
+
+# Find S(5).
+
+#Answer:
+    #209110240768
+
+from mathplus import timer
+
+N = 5
+
+
+@timer
+def pe(n):
+    start = tuple([0] * n + [1])
+    length = 2 ** n
+    pool = (0, 1)
+    answers = []
+
+    def bin_to_dec(t):
+        s = 0
+        for i in t:
+            s = s * 2 + i
+        return s
+
+    def test(start, pool):
+        if len(start) == length:
+            circle = start[1 - n:] + start[:n - 1]
+            for i in range(n - 1):
+                val = bin_to_dec(circle[i:i + n])
+                if val in pool:
+                    return
+                pool += (val,)
+            answers.append(bin_to_dec(start))
+        else:
+            val = bin_to_dec(start[1 - n:]) * 2
+            for i in range(2):
+                if val + i not in pool:
+                    test(start + (i,), pool + (val + i,))
+
+    test(start, pool)
+    return sum(answers)
+
+print(pe(N))
